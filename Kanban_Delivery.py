101dc84a56ae9e834adf307b27e215817c7f0477
Nondhawath <nondhawath.mindautomotive@gmail.com>
2026-01-15 15:56:33 +0700

    Update Kanban_Delivery.py

--- Kanban_Delivery.py
+++ Kanban_Delivery.py
@@ -286,9 +286,9 @@ elif mode == "Lot Kanban Summary":
 # =====================================================
 # 📦 KANBAN DELIVERY LOG (FINAL / OR SEARCH)
 # =====================================================
-elif mode == "📦 Kanban Delivery Log":
+elif mode == "Kanban Delivery Log":
 
-    st.header("📦 Kanban Delivery Log")
+    st.header("Kanban Delivery Log")
 
     # -----------------------------
     # SEARCH INPUT (OPTIONAL ALL)
@@ -460,6 +460,7 @@ elif mode == "Upload Lot Master":
     if file:
         df = pd.read_csv(file) if file.name.endswith(".csv") else pd.read_excel(file)
         st.dataframe(df.head())
+
 
 
 
